05ee3dab973be0a27896c813903ca2f217e32f63
Randa <muhamadrandap@gmail.com.com>
2023-11-09 14:23:25 +0700

    Latihan 2

---
+++ src/aplikasipertambahanduaagka/JOptionpane.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasipertambahanduaagka;
+
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author User
+ */
+class JOptionpane {
+
+    static boolean YES_OPTION;
+
+    static boolean showConfirmDialog(JRootPane rootPane, String tanya) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
